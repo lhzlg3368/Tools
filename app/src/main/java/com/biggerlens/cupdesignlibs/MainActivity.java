cb257a8fa388924f8189d2b50759960507389408
lhzlg3368 <lhzlg33686@hotmail.com>
2018-01-18 16:46:59 +0800

    en:add classpath 'com.jakewharton:butterknife-gradle-plugin:8.4.0' cn:添加 classpath 'com.jakewharton:butterknife-gradle-plugin:8.4.0'

--- app/src/main/java/com/biggerlens/cupdesignlibs/MainActivity.java
+++ app/src/main/java/com/biggerlens/cupdesignlibs/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //测试成功Library中引用的库文件都会在被别人做为依赖时添加到项目中
+        /*
+           并不会按照上面说的一样，
+           部分引用可以传递，
+           但是很多还是不可以的
+         */
     }
 
 }
